8239716b12a44fa76ae3d5d2859df77205803320
Fazakas Nimród <kriszterik1@gmail.com>
2024-10-03 19:02:02 +0300

    Extension functions

--- kotlin-exercises/labor-02/src/main/Date.kt
+++ kotlin-exercises/labor-02/src/main/Date.kt
@@ -13,3 +13,13 @@ data class Date(var year: Int = LocalDate.now().year,
     }
 }
 
+fun Date.isLeapYear() = (this.year % 4 == 0 && this.year % 100 != 0) || (this.year % 400 == 0)
+
+fun Date.isValid(): Boolean {
+    return try {
+        LocalDate.of(this.year, this.month, this.day)
+        true
+    } catch (e: Exception) {
+        false
+    }
+}
